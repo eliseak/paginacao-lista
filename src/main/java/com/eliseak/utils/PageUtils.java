07bc698998785084647240c89a22d68de9d38581
Elise <eliseakemi@gmail.com>
2019-03-25 17:05:26 -0300

    Rename variables

--- src/main/java/com/eliseak/utils/PageUtils.java
+++ src/main/java/com/eliseak/utils/PageUtils.java
@@ -14,13 +14,13 @@ public class PageUtils {
 	 * Calcula o total de paginas que uma lista possui dado um valor limite 
 	 * de itens por pagina
 	 * 
-	 * @param lista a lista que deve ser paginada
-	 * @param limite o limite de itens por pagina
+	 * @param list a lista que deve ser paginada
+	 * @param limit o limite de itens por pagina
 	 * 
 	 * @return numero total de paginas 
 	 */
-	public static Integer getTotalPages(List<?> lista, Integer limite) {
-		return (int) Math.ceil( lista.size()/ limite.doubleValue() );
+	public static Integer getTotalPages(List<?> list, Integer limit) {
+		return (int) Math.ceil( list.size()/ limit.doubleValue() );
 	}
 
 	/**
@@ -29,30 +29,32 @@ public class PageUtils {
 	 * O retorno é nulo quando o numero da pagina ultrapassa 
 	 * o total de paginas possiveis.
 	 * 
-	 * @param lista a lista que deve ser paginada
-	 * @param pagina a pagina desejada
-	 * @param limite o limite de itens por pagina
+	 * @param list a lista que deve ser paginada
+	 * @param page a pagina desejada
+	 * @param limit o limite de itens por pagina
 	 * 
 	 * @return lista com os itens pagina desejada
 	 */
 	public static List<?> getPageContent(
-			List<?> lista, Integer pagina, Integer limite){
+			List<?> list, Integer page, Integer limit){
 
-		if (!isValidPage(lista, pagina, limite)) {
+		if (!isValidPage(list, page, limit)) {
 			return null;
 		}
 
-		Integer inicio = ((pagina - 1) * limite);
+		// Variavel que indica o inicio da sublista
+		Integer fromIndex = ((page - 1) * limit);
 
-		Integer itens = inicio + limite;
+		// Variavel que indica o fim da sublista
+		Integer toIndex = fromIndex + limit;
 
 		// Código para evitar NullPointer no método de subList
-		if (pagina == getTotalPages(lista, limite)) {
-			itens = inicio + getCountLastPageItens(lista.size(), limite);
+		if (page == getTotalPages(list, limit)) {
+			toIndex = fromIndex + getCountLastPageItens(list.size(), limit);
 
 		}
 
-		List<?> subList = lista.subList(inicio, itens);
+		List<?> subList = list.subList(fromIndex, toIndex);
 
 		return subList;
 	}
@@ -61,19 +63,19 @@ public class PageUtils {
 	/**
 	 * Verifica se a pagina requerida e valida
 	 * 
-	 * @param lista lista a ser paginada
-	 * @param pagina numero da pagina desejada
-	 * @param limite numero de itens por pagina
+	 * @param list lista a ser paginada
+	 * @param page numero da pagina desejada
+	 * @param limit numero de itens por pagina
 	 */
 	private static boolean isValidPage(
-			List<?> lista, Integer pagina, Integer limite) {
+			List<?> list, Integer page, Integer limit) {
 
-		if (pagina <= 0) {
+		if (page <= 0) {
 			return false;
 		}
 
-		Integer totalPaginas = getTotalPages (lista, limite);
-		if (pagina > totalPaginas) {
+		Integer totalPages = getTotalPages (list, limit);
+		if (page > totalPages) {
 			return false;
 		}
 
@@ -84,11 +86,11 @@ public class PageUtils {
 	 * Retorna quantos itens teria a ultima pagina da lista
 	 * 
 	 * @param total total de itens da lista
-	 * @param limite limite de itens por pagina
+	 * @param limit limite de itens por pagina
 	 */
 	private static Integer getCountLastPageItens(
-			Integer total, Integer limite) {
-		return total % limite;
+			Integer total, Integer limit) {
+		return total % limit;
 	}
 
 }
